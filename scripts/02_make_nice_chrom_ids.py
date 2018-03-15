bfb5045794c62ed9a848a53c1fcf9fb98fb8053d
Rob <schae234@umn.edu>
2018-03-15 14:11:04 -0500

    working version

--- scripts/02_make_nice_chrom_ids.py
+++ scripts/02_make_nice_chrom_ids.py
@@ -1,13 +1,17 @@
 
+import minus80 as m80
 import locuspocus as lp
-ids = dict()  
 
-with open('../ensemble_id_map.txt','r') as IN:                  
+ids = dict()  
+with open('ensemble_id_map.txt','r') as IN:                  
     for line in IN:                                          
         i,j = line.strip().split(',')                        
         ids[i] = j     
+if m80.Tools.available('EquCab3',dtype='Fasta'): 
+    ec3 = lp.Fasta('EquCab3')
+else:
+    ec3 = lp.Fasta.from_file('EquCab3','EquCab3.fa.gz')
 
-ec3 = lp.Fasta('EquCab3')
 with open('EquCab3_nice_chrom_ids.fasta','w') as OUT:                                                                                                        
     for chrom_name in ec3.chrom_names():                                        
         chrom = ec3[chrom_name]                                                 
@@ -15,6 +19,6 @@ with open('EquCab3_nice_chrom_ids.fasta','w') as OUT:
         if easy_id == 'chrUn':
             easy_id = easy_id + '_' + chrom_name
         print(f'>{easy_id} {chrom_name} {" ".join(chrom._attrs)}',file=OUT)     
-        for i in range(0,len(chrom),50):                                        
-            print(''.join(chrom.seq[i:i+50]),file=OUT) 
+        for i in range(0,len(chrom),70):                                        
+            print(''.join(chrom.seq[i:i+70]),file=OUT) 
 
