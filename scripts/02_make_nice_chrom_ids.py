96707dda08ccf0b36bc3947f8add1d4d02a87010
Rob <schae234@umn.edu>
2018-03-26 15:53:39 -0500

    change spaces to tabs in gff

--- scripts/02_make_nice_chrom_ids.py
+++ scripts/02_make_nice_chrom_ids.py
@@ -1,24 +1,44 @@
-
+import os 
 import minus80 as m80
 import locuspocus as lp
+
+from minus80.RawFile import RawFile
 
 ids = dict()  
 with open('ensemble_id_map.txt','r') as IN:                  
     for line in IN:                                          
-        i,j = line.strip().split(',')                        
+        i,j = line.strip().split()                        
         ids[i] = j     
 if m80.Tools.available('EquCab3',dtype='Fasta'): 
     ec3 = lp.Fasta('EquCab3')
 else:
     ec3 = lp.Fasta.from_file('EquCab3','EquCab3.fa.gz')
 
-with open('EquCab3_nice_chrom_ids.fasta','w') as OUT:                                                                                                        
-    for chrom_name in ec3.chrom_names():                                        
-        chrom = ec3[chrom_name]                                                 
-        easy_id = ids[chrom_name]                                               
-        if easy_id == 'chrUn':
-            easy_id = easy_id + '_' + chrom_name
-        print(f'>{easy_id} {chrom_name} {" ".join(chrom._attrs)}',file=OUT)     
-        for i in range(0,len(chrom),70):                                        
-            print(''.join(chrom.seq[i:i+70]),file=OUT) 
+if not os.path.exists('EquCab3_nice.fasta'):
+    with open('EquCab3_nice.fasta','w') as OUT:                                                                                                        
+        for chrom_name in ec3.chrom_names():                                        
+            print(f'Printing out {chrom_name}')
+            chrom = ec3[chrom_name]                                                 
+            # try to get easy_id
+            if chrom_name in ids:
+                chrom_name = f"{ids[chrom_name]} {chrom_name}"
+            # Keep track of lengths 
+            start_length = len(chrom)
+            printed_length = 0
+            # Loop and print
+            print(f'>{chrom_name} {" ".join(chrom._attrs)}',file=OUT)     
+            for i in range(0,len(chrom),70):                                        
+                sequence = chrom.seq[i:i+70]
+                print(''.join(sequence),file=OUT) 
+                printed_length += len(sequence)
+            if printed_length != start_length:
+                raise ValueError('Chromosome was truncated during printing')
+
+if not os.path.exists('EquCab3_nice.gff'):
+    with RawFile('GCF_002863925.1_EquCab3.0_genomic.gff.gz') as IN, open('EquCab3_nice.gff','w') as OUT:
+        for line in IN:
+            id,*fields = line.split()
+            if id in ids:
+                id = ids[id]
+            print(id,*fields,file=OUT,sep='\t')
 
